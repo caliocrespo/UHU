77dab53ed0630ffcae5ca0494f4e2a22a0eb486f
Kamisama <Kamisama@GOD>
2022-10-10 01:22:47 +0200

    nueva variable

--- pr1/src/pr1/adios.java
+++ pr1/src/pr1/adios.java
@@ -5,6 +5,9 @@ public class adios {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("adios");
+		String despedida= "que vaya bien";
+		System.out.println(despedida);
+		
 	}
 
 }
