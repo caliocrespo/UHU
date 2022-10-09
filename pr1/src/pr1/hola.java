7086f54800a7c62f074451c31e3bc1081e848d7e
Kamisama <Kamisama@GOD>
2022-10-10 01:15:11 +0200

    completando el saludo

--- pr1/src/pr1/hola.java
+++ pr1/src/pr1/hola.java
@@ -5,7 +5,8 @@ public class hola {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hola");
-		String saludo;
+		String saludo="buenas tardes a todos";
+		System.out.println(saludo);
 	}
 
 }
